83c821d104fef34072cc0d8a7b7d0c26cfd58f15
bell-ryan-w <bell.ryan.w@gmail.com>
2015-09-15 16:17:38 -0400

    new task requires priority input

--- todo-src/script.js
+++ todo-src/script.js
@@ -16,10 +16,15 @@ myApp.controller('MainCtrl', function ($scope){
   $scope.addItem = function(){
     console.log("in add");
     if ($scope.newItem !== ""){
+      if($scope.priority == ""){
+        alert("Please input a priority.");
+      }
+      else{
       $scope.todos.push({task:$scope.newItem, status:"incomplete"});
       $scope.newItem = "";
         $scope.priorities.push($scope.priority);
       $scope.priority = "";
+      }
     }
   }
 
@@ -29,7 +34,7 @@ myApp.controller('MainCtrl', function ($scope){
     $scope.todos.splice(index, 1);
       $scope.priorities.splice(index, 1);
   }
-  
+
   $scope.deleteCompleted = function(index){
   if(index > -1){
     if($scope.todos[index].status == 'complete'){
